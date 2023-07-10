4c3f7159eb46552233e374b44714bf555b5f2dba
unknown <jerez4959@gmail.com>
2023-07-10 00:42:13 -0400

    1004

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const next = require('next');
-const { join } = require('path');
+// const express = require('express');
+// const next = require('next');
+// const { join } = require('path');
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+// app.prepare().then(() => {
+//   const server = express();
 
-  // Sirve los archivos estáticos desde la carpeta "public"
+//   // Sirve los archivos estáticos desde la carpeta "public"
 
-  server.use('/uploads', express.static(join(__dirname, 'public/uploads')));
+//   server.use('/uploads', express.static(join(__dirname, 'public/uploads')));
 
 
-  // Maneja todas las demás rutas usando Next.js
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
+//   // Maneja todas las demás rutas usando Next.js
+//   server.all('*', (req, res) => {
+//     return handle(req, res);
+//   });
 
-  // Inicia el servidor
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
-});
+//   // Inicia el servidor
+//   server.listen(3000, (err) => {
+//     if (err) throw err;
+//     console.log('> Ready on http://localhost:3000');
+//   });
+// });
 
