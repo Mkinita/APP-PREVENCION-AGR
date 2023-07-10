e60472764aadb6e3a137cd6f869535143c86d458
unknown <jerez4959@gmail.com>
2023-07-09 21:59:20 -0400

    9

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const next = require('next');
+const { join } = require('path');
 
-const app = express();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.use('/uploads', express.static('./public/uploads'));
+app.prepare().then(() => {
+  const server = express();
 
-const PORT = process.env.PORT || 5486;
+  // Sirve los archivos estáticos desde la carpeta "public"
+  server.use('/uploads', express.static(join(__dirname, 'public/uploads')));
 
-app.listen(PORT);
-console.log('funcionando en el puerto', PORT);
+
+  // Maneja todas las demás rutas usando Next.js
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  // Inicia el servidor
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
+
