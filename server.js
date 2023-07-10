b987847b56e7f1b9ffcb046b8bd1d7de5f5742b8
unknown <jerez4959@gmail.com>
2023-07-09 22:46:56 -0400

    100

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.prepare().then(() => {
   const server = express();
 
   // Sirve los archivos estáticos desde la carpeta "public"
-  server.use('/uploads', express.static(join(__dirname, '.next/public/uploads')));
 
+  server.use('/uploads', express.static(join(__dirname, 'public/uploads')));
 
 
   // Maneja todas las demás rutas usando Next.js
