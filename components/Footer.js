545437d968cd349e0ea8558054ae754a8868a133
unknown <jerez4959@gmail.com>
2023-07-11 13:11:42 -0400

    provandos

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 text-center py-3">
+    <footer className="bg-white text-center py-24 pb-0">
       <p className="text-xs font-bold">Desarrollado por Carlos Jerez Flores 👨‍💻</p>
     </footer>
   );
